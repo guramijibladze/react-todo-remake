e1d3e58818ff9277be55e883091a4c968c5ce45a
guramijibladze <guramijibladze@gmail.com>
2020-09-05 12:09:09 +0400

    up

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -4,14 +4,15 @@ import Form from './components/Form.js';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [ inputText, setInputText ] = useState("");
+  const [ todos, setTodos ] = useState([]);
 
-  
   return (
     <div className="App">
       <header>
-        <h1>Ed's Todo List</h1>
+        <h1>Ed's Todo List </h1>
       </header>
-      <Form />
+      <Form setInputText={ setInputText } />
       <TodoList />
     </div>
   );
